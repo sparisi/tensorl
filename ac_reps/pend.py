policy kl renamed to klm (m-projection), and added kli (i-projection) removed "fast deterministic policy" from reps (it was wrong)

ac_reps/pend.py
@@ -144,7 +144,7 @@ def main(seed=1, run_name=None):
         session.run(update_std, {new_std_ph: np.sqrt(np.diag(new_cov))[None,:]*1.001})
 
         end_neg_lik = session.run(loss_pi, {pi.obs: paths["obs"], pi.act: paths["act"], weights: w[:,None]})
-        actual_kl = pi.estimate_kl(paths["obs"], old_mean, old_std)
+        actual_kl = pi.estimate_klm(paths["obs"], old_mean, old_std)
 
         # Plot V, REPS weights, and pi
         ax_scatter.cla()
@@ -155,14 +155,8 @@ def main(seed=1, run_name=None):
         myplot_pi.update(session.run(mean.output[0], {obs: np.atleast_2d(XY)}))
         myplot_pi2.update(np.clip(session.run(mean.output[0], {obs: np.atleast_2d(XY)}), -2, 2))
 
-        # Fast deterministic policy
-        bw = np.atleast_2d(bw)
-        P = session.run(mean.P)
-        shift = session.run(mean.shift)
-        pi_det = lambda x: ( np.sin( (np.atleast_2d(x)/bw).dot(P) + shift) ).dot(new_K.T[1:,:]) + new_K.T[0,:]
-
         # Evaluate pi and print info
-        avg_rwd = evaluate_policy(env_eval, policy=pi_det, min_paths=1000)
+        avg_rwd = evaluate_policy(env_eval, policy=pi.draw_action_det, min_paths=1000)
         # avg_rwd = np.sum(paths["rwd"]) / paths["nb_paths"]
         entr = pi.estimate_entropy(paths["obs"])
         print('%d | %.4f, %.4f, %.4f (%.4f), %.4f -> %.4f' % (itr, avg_rwd, entr, kl, actual_kl, init_neg_lik, end_neg_lik), flush=True)

ac_reps/solver.py
@@ -20,7 +20,7 @@ class ACREPS:
         self.v = v
         self.theta = v.vars
         self.adv = self.q - self.v.output[0]
-        self.w = tf.exp(1./self.eta * (self.adv - tf.reduce_max(self.adv)))
+        self.w = tf.exp((self.adv - tf.reduce_max(self.adv)) / self.eta)
         self.dual = self.eta*self.epsilon + self.eta*tf.log(tf.reduce_mean(self.w)) + tf.reduce_max(self.adv) + tf.reduce_mean(self.v.output[0])
         self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.dual,
                                               options={'maxiter': scipy_iter, 'disp': False, 'ftol': 0},

common/policy.py
@@ -48,9 +48,14 @@ class MVNPolicy:
 
         self.old_mean = tf.placeholder(dtype=obs.dtype, shape=[None, self.act_size], name=name+'_old_mean')
         self.old_std = tf.placeholder(dtype=obs.dtype, shape=[None, self.act_size], name=name+'_old_std')
-        self.kl = 0.5 * tf.reduce_mean(2.*tf.reduce_sum(tf.log(self.std), axis=1) - 2.*tf.reduce_sum(tf.log(self.old_std), axis=1) +
+
+        # KL divergences: m-projection KL(pi_new || pi_old) and i-projection KL(pi_old || pi_new)
+        self.klm = 0.5 * tf.reduce_mean(2.*tf.reduce_sum(tf.log(self.std), axis=1) - 2.*tf.reduce_sum(tf.log(self.old_std), axis=1) +
                                 tf.reduce_sum((self.mean - self.old_mean)**2 / self.std**2, axis=1) +
                                 tf.reduce_sum(self.old_std**2/self.std**2, axis=1) - self.act_size)
+        self.kli = 0.5 * tf.reduce_mean(2.*tf.reduce_sum(tf.log(self.old_std), axis=1) - 2.*tf.reduce_sum(tf.log(self.std), axis=1) +
+                                tf.reduce_sum((self.old_mean - self.mean)**2 / self.old_std**2, axis=1) +
+                                tf.reduce_sum(self.std**2/self.old_std**2, axis=1) - self.act_size)
 
 
     def get_log_prob(self, obs, act):
@@ -59,8 +64,11 @@ class MVNPolicy:
     def estimate_entropy(self, obs):
         return np.squeeze(self.session.run(self.entropy, {self.obs: np.atleast_2d(obs)}))
 
-    def estimate_kl(self, obs, old_mean, old_std):
-        return np.squeeze(self.session.run(self.kl, {self.obs: np.atleast_2d(obs), self.old_std: np.atleast_2d(old_std), self.old_mean: np.atleast_2d(old_mean)}))
+    def estimate_kli(self, obs, old_mean, old_std):
+        return np.squeeze(self.session.run(self.kli, {self.obs: np.atleast_2d(obs), self.old_std: np.atleast_2d(old_std), self.old_mean: np.atleast_2d(old_mean)}))
+
+    def estimate_klm(self, obs, old_mean, old_std):
+        return np.squeeze(self.session.run(self.klm, {self.obs: np.atleast_2d(obs), self.old_std: np.atleast_2d(old_std), self.old_mean: np.atleast_2d(old_mean)}))
 
     def draw_action(self, obs):
         return np.squeeze(self.session.run(self.output, {self.obs: np.atleast_2d(obs)}))

reps/reps.py
@@ -140,17 +140,11 @@ def main(env_name, seed=1, run_name=None):
         session.run(update_std, {new_std_ph: np.sqrt(np.diag(new_cov))[None,:]})
 
         end_neg_lik = session.run(loss_pi, {pi.obs: np.atleast_2d(paths["obs"]), pi.act: np.atleast_2d(paths["act"]), weights: w[:,None]})
-        actual_kl = pi.estimate_kl(paths["obs"], old_mean, old_std)
-
-        # Fast deterministic policy
-        bw = np.atleast_2d(bw)
-        P = session.run(mean.P)
-        shift = session.run(mean.shift)
-        pi_det = lambda x: ( np.sin( (np.atleast_2d(x)/bw).dot(P) + shift) ).dot(new_K.T[1:,:]) + new_K.T[0,:]
+        actual_kl = pi.estimate_klm(paths["obs"], old_mean, old_std)
 
         # Evaluate pi and print info
-        #avg_rwd = evaluate_policy(env_eval, policy=pi_det, min_paths=paths_eval)
-        avg_rwd = np.sum(paths["rwd"]) / paths["nb_paths"]
+        avg_rwd = evaluate_policy(env_eval, policy=pi.draw_action_det, min_paths=paths_eval)
+        # avg_rwd = np.sum(paths["rwd"]) / paths["nb_paths"]
         entr = pi.estimate_entropy(paths["obs"])
         print('%d | %.4f, %.4f, %.4f (%.4f), %.4f -> %.4f' % (itr, avg_rwd, entr, kl, actual_kl, init_neg_lik, end_neg_lik), flush=True)
         if verbose:

trpo/trpo.py
@@ -65,7 +65,7 @@ def main(env_name, seed=1, run_name=None):
     old_log_probs = tf.placeholder(dtype=precision, shape=[None, 1], name='old_log_probs')
     prob_ratio = tf.exp(pi.log_prob - old_log_probs)
     loss_pi = -tf.reduce_mean(tf.multiply(prob_ratio, advantage))
-    solver = TRPO(session, loss_pi, pi.kl, mean.vars+[std], kl_bound=kl_bound, cg_damping=cg_damping)
+    solver = TRPO(session, loss_pi, pi.klm, mean.vars+[std], kl_bound=kl_bound, cg_damping=cg_damping)
 
     # Init variables
     session.run(tf.global_variables_initializer())
@@ -111,7 +111,7 @@ def main(env_name, seed=1, run_name=None):
         # avg_rwd = evaluate_policy(env, policy=draw_fast, min_paths=paths_eval)
         avg_rwd = np.sum(paths["rwd"]) / paths["nb_paths"]
         entr = pi.estimate_entropy(paths["obs"])
-        kl = pi.estimate_kl(paths["obs"], old_mean, old_std)
+        kl = pi.estimate_klm(paths["obs"], old_mean, old_std)
         print('%d | %e -> %e   %e -> %e   %e   %e   %e   %e   ' % (itr, v_loss_before, v_loss_after, pi_loss_before, pi_loss_after, entr, kl, avg_rwd, mstde), flush=True)
         with open(logger.fullname, 'ab') as f:
             np.savetxt(f, np.atleast_2d([v_loss_before, v_loss_after, pi_loss_before, pi_loss_after, entr, kl, avg_rwd, mstde])) # save data